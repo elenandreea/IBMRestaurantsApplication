41804c7a7a017e2765b2321f3325308cb78c6e48
andreea <andreeagorgoi@gmail.com>
2020-10-28 16:05:08 +0200

    day5 added animation on splash screen

--- app/src/main/java/com/ibm/restaurants/SplashActivity.java
+++ app/src/main/java/com/ibm/restaurants/SplashActivity.java
@@ -1,19 +1,40 @@
 package com.ibm.restaurants;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.ibm.restaurants.restaurants.RestaurantsActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    AppCompatImageView logo, appName, splashImg;
+    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Intent intent = new Intent(this, RestaurantsActivity.class);
-        startActivity(intent);
+
+        logo = findViewById(R.id.logo);
+        appName = findViewById(R.id.app_name);
+        splashImg = findViewById(R.id.image_intro);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        appName.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, RestaurantsActivity.class);
+            startActivity(intent);
+            finish();
+        }, 4000);
+
     }
 }
